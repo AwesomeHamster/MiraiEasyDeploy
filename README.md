8e10e9b063abff55bd33acfb4d9d2d27e92b4156
Maiko Tan <maiko.tan.coding@gmail.com>
2020-07-26 18:24:28 +0800

    ci: add test workflows

---
+++ README.md
@@ -0,0 +1,3 @@
+# MiraiOK Runner
+
+A too young too simple runner of [MiraiOK](https://github.com/LXY1226/MiraiOK), to make it easier to deploy onto server.
---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# download MiraiOK
+if [ ! -e MiraiOK ]
+then
+    wget -c http://t.imlxy.net:64724/mirai/MiraiOK/miraiOK_linux_amd64
+    mv miraiOK_linux_amd64 MiraiOK
+fi
+
+# download plugins
+if [ ! -d "plugins" ]
+then
+    mkdir -p plugins
+fi
+
+if [ ! -e "plugins/mirai-api-http.jar" ]
+then
+    wget -c https://github.com/project-mirai/mirai-plugins/raw/master/mirai-api-http/mirai-api-http-1.7.0.jar -P plugins/
+    mv mirai-api-http-1.7.0.jar plugins/mirai-api-http.jar
+fi
+
+# create config.txt
+echo -e "----------\nlogin $1\nsay $2 $3" > config.txt
