6818816ac70bf71d6f1f4b01ea7857973e9f607c
Maiko Tan <maiko.tan.coding@gmail.com>
2020-07-29 20:52:49 +0800

    feat: add environment variable check

--- run.sh
+++ run.sh
@@ -18,7 +18,18 @@ then
 fi
 
 # create config.txt
-echo -e "----------\nlogin ${MIRAI_BOT_INFO}\nsay ${MIRAI_TEST_GROUP} ${MIRAI_TEST_MESSAGE}" > config.txt
+if [[ -z ${MIRAI_BOT_INFO} ]]
+then
+    echo -e "----------\nlogin ${MIRAI_BOT_INFO}" > config.txt
+else
+    echo "no environment variable MIRAI_BOT_INFO defined, exiting..."
+    exit 1
+fi
+
+if [[ -z ${MIRAI_TEST_GROUP} && ${MIRAI_TEST_MESSAGE} ]]
+then
+    echo "say ${MIRAI_TEST_GROUP} ${MIRAI_TEST_MESSAGE}" >> config.txt
+fi
 
 # run MiraiOK
 chmod +x ./MiraiOK
