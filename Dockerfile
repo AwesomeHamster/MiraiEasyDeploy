03dc96375d592f07af51e71f7d2ef57209ba2602
Maiko Tan <maiko.tan.coding@gmail.com>
2020-07-29 17:46:10 +0800

    chore: clean up dockerfile / docker-compose.yml
    - add Dockerfile
    - add base.env
    - move apt-get to Dockerfile
    - prefix 'MIRAI_' to all environment variables

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM debian:buster
+
+# set tsinghua tuna debian mirror server
+# uncomment it in mainland china
+RUN mv /etc/apt/sources.list /etc/apt/sources.list.backup \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free" > /etc/apt/sources.list \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-updates main contrib non-free" >> /etc/apt/sources.list \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-backports main contrib non-free" >> /etc/apt/sources.list \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian-security buster/updates main contrib non-free" >> /etc/apt/sources.list
+
+# install neccesary softwares
+RUN apt-get update \
+    && apt-get install -y wget
--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 # download MiraiOK
 if [ ! -e MiraiOK ]
 then
-    wget -c http://t.imlxy.net:64724/mirai/MiraiOK/miraiOK_linux_amd64
-    mv miraiOK_linux_amd64 MiraiOK
+    wget -c ${MIRAI_DOWNLOAD_URL} -O MiraiOK
 fi
 
 # download plugins
@@ -15,13 +14,12 @@ fi
 
 if [ ! -e "plugins/mirai-api-http.jar" ]
 then
-    wget -c https://github.com/project-mirai/mirai-plugins/raw/master/mirai-api-http/mirai-api-http-1.7.0.jar -P plugins/
-    mv mirai-api-http-1.7.0.jar plugins/mirai-api-http.jar
+    wget -c ${MIRAI_API_HTTP_DOWNLOAD_URL} -O plugins/mirai-api-http.jar
 fi
 
 # create config.txt
-echo -e "----------\nlogin $1\nsay $2 $3" > config.txt
+echo -e "----------\nlogin ${BOT_INFO}\nsay ${TEST_GROUP} ${TEST_MESSAGE}" > config.txt
 
 # run MiraiOK
 chmod +x ./MiraiOK
-./MiraiOK -test.v -test.run ^$
+./MiraiOK
